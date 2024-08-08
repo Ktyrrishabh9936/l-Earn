048e7ba15a6e37de6df31239c96d5de2e6aab3bc
Rishabh <ktyrrishabh99361032@gmail.com>
2024-08-08 14:15:44 +0530

    animate

--- src/components/MainheaderSecton.js
+++ src/components/MainheaderSecton.js
@@ -3,19 +3,19 @@ import { PiTranslateBold } from 'react-icons/pi';
 
 export default function MainSection() {
   return (
-    <div className='min-h-[80vh] flex justify-center items-center pb-28' style={{backgroundColor:'rgba(29,13,207,1)'}}>
-      <div className=" text-6xl sm:text-[clamp(5rem,8vw,9rem)] font-extrabold  text-center font-Inter leading-[4rem] md:leading-[5rem] xl:leading-[8rem] text-white relative ">
-      <div className="rounded-md  backdrop-blur-sm bg-[rgba(255,255,255,0.5)]  absolute -bottom-28 left-16 px-6 pt-6 pb-10 -rotate-[30deg] z-10"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0zS2xtkqCWnBYJwt0VgD5aVrrw7jkATAZAQ&s" className='rounded-full h-16 w-16' alt="" /></div>
-      <div className="rounded-md bg-[rgba(255,255,255,0.5)] absolute -top-16 sm:-top-16 xl:top-24 right-12 xl:right-36 px-4 pt-4 pb-8 rotate-[28deg] z-10 backdrop-blur-sm"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0zS2xtkqCWnBYJwt0VgD5aVrrw7jkATAZAQ&s" className='rounded-full h-12 w-12' alt="" /></div>
-        <div className=' relative flex flex-col z-0 '>
+    <div className='min-h-[90vh] bg-image-animate flex justify-center items-center pb-28' >
+      <div className=" text-6xl sm:text-[clamp(5rem,8vw,9rem)] font-extrabold  text-center font-Inter  text-white relative ">
+      {/* <div className="rounded-md  backdrop-blur-sm bg-[rgba(255,255,255,0.5)]  absolute -bottom-28 left-16 px-6 pt-6 pb-10 -rotate-[30deg] z-10"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0zS2xtkqCWnBYJwt0VgD5aVrrw7jkATAZAQ&s" className='rounded-full h-16 w-16' alt="" /></div>
+      <div className="rounded-md bg-[rgba(255,255,255,0.5)] absolute -top-16 sm:-top-16 xl:top-24 right-12 xl:right-36 px-4 pt-4 pb-8 rotate-[28deg] z-10 backdrop-blur-sm"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0zS2xtkqCWnBYJwt0VgD5aVrrw7jkATAZAQ&s" className='rounded-full h-12 w-12' alt="" /></div> */}
+        <div className=' relative flex flex-col z-0 leading-[4rem] md:leading-[5rem] xl:leading-[7rem]'>
         <span>Master</span>
         <span>New</span>
         <span>Languages</span>
         </div>
-          <img className='h-[clamp(10rem,13vw,14rem)] absolute -top-8 md:-top-2 xl:top-10 -right-3 md:right-0 xl:right-16' src='svg/arrow.svg'></img>
-          <p className='absolute -top-8 -left-7 lg:left-4 xl:left-16'><PiTranslateBold style={{color:'#F8C045'}} fontSize={100}/></p>
+          <img className='h-[clamp(10rem,11.5vw,12.5rem)] absolute -top-8 md:-top-2 xl:top-10 -right-3 md:right-0 xl:right-20' src='svg/arrow.svg'></img>
+          <p className='absolute -top-8 -left-7 lg:left-4 xl:left-12'><PiTranslateBold style={{color:'#F8C045'}} fontSize={100}/></p>
           
-          <div className="bg-yellow-500 rounded-full h-24  w-24  mb-4 absolute hidden sm:block -bottom-24 xl:-bottom-16  -right-12"></div>
+          {/* <div className="bg-yellow-500 rounded-full h-24  w-24  mb-4 absolute hidden sm:block -bottom-24 xl:-bottom-16  -right-12"></div> */}
       </div>
     </div>
   )
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react'
+import React, {  useEffect, useState } from 'react'
 import { AiFillMessage } from 'react-icons/ai';
 import { BsThreeDots } from 'react-icons/bs';
 import { IoMdClose } from 'react-icons/io';
@@ -6,6 +6,22 @@ import { MdMenuOpen } from 'react-icons/md';
 export default function Navbar() {
   const [isOpen,setisOpen] = useState(false);
   const navlinks = ['Home','Languages Courses','About Us','Contact Us'];
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
     function openSideBar(){
         setisOpen(!isOpen);
         document.body.style.height = '100vh';
@@ -16,22 +32,17 @@ export default function Navbar() {
         document.body.style.height = 'auto';
         document.body.style.overflowY = 'scroll';
       }
-
-      
   return (
-    <div className='w-[100vw]  py-6 whitespace-nowrap  font-Poppins' style={{backgroundColor:'rgba(29,13,207,1)'}}>
-      <div className="container mx-auto flex justify-between">
+    
+    <div className={`w-[100vw] py-3 whitespace-nowrap  font-Poppins drop-nav-shadow sticky top-0 z-50  transition-colors duration-300 ${
+        isScrolled ? "bg-[rgb(29,13,207)]" : "bg-[rgb(0,0,0)]"
+      }` }>
+      <div className="w-[97%] sm:w-[94%] md:w-[88%] lg:w-[84%] xl:w-[80%] mx-auto flex justify-between">
       <div className=" flex px-2 gap-7  justify-between  ">
         <p className='flex'>
       
-        <div className="flex items-center gap-1 text-clamp-h4">
-          <p className=' text-yellow-500'>
-          <AiFillMessage  fontSize={50}/>
-          </p>
-          <div className="  text-white text-2xl leading-6 font-ChakraPetch font-bold">
-                <p>L-Earn</p>
-                <p>Academy</p>
-          </div>
+        <div className="flex items-center gap-1 ">
+          <img src="img/LearnAcademy-logo.png" alt="" className=' h-12 md:h-16'/>
         </div>
         </p>
         </div>
@@ -49,7 +60,7 @@ export default function Navbar() {
           }}
         />
       </div>
-      <div className=" md:block hidden my-auto">
+      <div className=" md:block hidden my-auto text-sm">
         <ul className="flex  gap-7 py-2 text-white ">
           {navlinks.map((navlink) => {
           return <li className=' capitalize'>{navlink}</li>
@@ -58,7 +69,6 @@ export default function Navbar() {
       </div>
        <div className='hidden md:block my-auto'> 
         <div className="flex gap-8">
-       <span className=' h-min  px-2 rounded-full bg-slate-100 my-auto'><BsThreeDots fontSize={30}/></span>
        <span className='px-4 py-2 rounded-full bg-yellow-500 font-semibold h-min'>Apply Now</span>
        </div>
         </div>
